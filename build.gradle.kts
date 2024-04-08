2cb6050c4d90f12c024ca758276b2f495cb53c77
Nicolas Lukita <nicolas.lukita@gmail.com>
2024-04-09 00:33:13 +0800

    build: add jjwt and security dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,13 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+	implementation("io.jsonwebtoken:jjwt-api:0.12.5")
+	implementation("io.jsonwebtoken:jjwt-impl:0.12.5")
+	implementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
+
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<KotlinCompile> {
--- src/main/kotlin/com/nicolaslu/splitter/SplitterApplication.kt
+++ src/main/kotlin/com/nicolaslu/splitter/SplitterApplication.kt
@@ -1,9 +1,9 @@
 package com.nicolaslu.splitter
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
-
-@SpringBootApplication
+@SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
 class SplitterApplication
 
 fun main(args: Array<String>) {
